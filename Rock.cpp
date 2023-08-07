30886d531ccd97b67bd8a4eacb43cbf4cb5410ce
kjcndjkcnxjk <100855670+kjcndjkcnxjk@users.noreply.github.com>
2023-08-07 21:28:21 +0800

    Update Rock.cpp

--- Rock.cpp
+++ Rock.cpp
@@ -3,7 +3,8 @@
 Rock::Rock():Move("Rock"){}
 
 void Rock::compare(Move* move){
-    if(move->getName()=="Scissors"){
-        flag=1;
-    }
+    if (move->getName() == "Scissors")
+        flag = 1;
+    else if(move->getName() == "Rock")
+        flag = 2;
 }
